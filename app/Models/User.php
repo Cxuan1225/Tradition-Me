ea0129179d7f7b7027ee074d06b893d14191ff60
Cxuan1225 <tamhaojie1225@gmail.com>
2025-04-20 01:55:23 +0800

    Update laravel version

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -68,7 +69,7 @@ class User extends Authenticatable
     public function profileImage()
     {
         if ($this->profile_image) {
-            return asset('storage/'.$this->profile_image);
+            return asset('storage/' . $this->profile_image);
         }
 
         return asset('default-profile.png');
